789ca9a6a64eba3a6f08d3de1d3f5ca36ae20092
hide27k <hidehusky1996@gmail.com>
2020-12-12 15:12:28 -0800

    Add an arrow into the banner.

--- front-end/src/Banner.js
+++ front-end/src/Banner.js
@@ -6,6 +6,9 @@ function Banner() {
     <div id="banner">
       <h1 id="title">Music Style Transfer</h1>
       <p id="name">Paul Yoo, Sherry Yang, and Hideyuki Komaki</p>
+      <a href="#introduction">
+        <div class="arrow"></div>
+      </a>
       <video autoPlay loop muted playsInline src={movie} id="video"></video>
     </div>
   )
