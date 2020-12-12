f37efb4530215bb144eba7518cd01083dde9c8c9
hide27k <hidehusky1996@gmail.com>
2020-12-12 15:00:18 -0800

    Implement summary section (draft).

---
+++ front-end/src/Banner.js
@@ -0,0 +1,14 @@
+import movie from './banner.mov'
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div id="banner">
+      <h1 id="title">Music Style Transfer</h1>
+      <p id="name">Paul Yoo, Sherry Yang, and Hideyuki Komaki</p>
+      <video autoPlay loop muted playsInline src={movie} id="video"></video>
+    </div>
+  )
+}
+
+export default Banner;
---
+++ front-end/src/Demo.js
@@ -0,0 +1,11 @@
+import './Demo.css';
+
+function Demo() {
+  return (
+    <div id="demo">
+      <h2>Music Style Transfer Demo</h2>
+    </div>
+  )
+}
+
+export default Demo;
