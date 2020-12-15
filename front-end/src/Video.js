3404b60e52338c3b78739f07ced1ba9f39e54064
hide27k <hidehusky1996@gmail.com>
2020-12-14 22:19:31 -0800

    Add all contents.

--- front-end/src/Video.js
+++ front-end/src/Video.js
@@ -4,6 +4,7 @@ function Video() {
   return (
     <div id="movie">
       <h2>Movie</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/-7oCCaz48f4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   )
 }
