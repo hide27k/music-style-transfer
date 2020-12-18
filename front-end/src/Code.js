e01f046444ae1463951d2a92ac675be02a96b6c1
hide27k <hidehusky1996@gmail.com>
2020-12-17 21:47:19 -0800

    Add source codes.

--- front-end/src/Code.js
+++ front-end/src/Code.js
@@ -4,6 +4,12 @@ function Code() {
   return (
     <div id="code">
       <h2>Source Code</h2>
+      <p>
+      The source code of our model is available on <a href="https://colab.research.google.com/drive/1wIzk3mAcrKap6fmqHUNRdBr6q6p02tlj?usp=sharing" target="__blank">Google Colab</a>.
+      </p>
+      <p>
+      You can also clone our code through this <a href="https://github.com/hide27k/music-style-transfer" target="__blank" >GitHub repository.</a>
+      </p>
     </div>
   )
 }
